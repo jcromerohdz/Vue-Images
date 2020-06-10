46c4f09ac16da5fcafbb525c8260250bde77af90
J. Christian Romero <jcromerohdz@gmail.com>
2020-06-09 17:34:48 -0700

    Some conditions to see if the user is logged in

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,31 +1,33 @@
 <template>
-  <div class="image-container">
-    <img v-for="image in allImages" :src="image.link" :key="image.id">
+  <div>
+    <div v-if="isLoggedIn" class="image-container">
+      <img v-for="image in allImages" :src="image.link" :key="image.id" />
+    </div>
 
+    <h2 v-else>Log in to get started!</h2>
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapGetters } from "vuex";
 
 export default {
-  name: 'ImageList',
-  computed: mapGetters(['allImages']),
-  methods: mapActions( ['fetchImages']),
-  created () {
-    this.fetchImages()
-  }
-}
+  name: "ImageList",
+  computed: mapGetters(["allImages", "isLoggedIn"]),
+  methods: mapActions(["fetchImages"]),
+  created() {
+    this.fetchImages();
+  },
+};
 </script>
 
 <style scoped>
-  .image-container {
-    column-count: 3;
-    column-gap: 0;
-  }
-  img {
-    max-width: 100%;
-    padding: 5px;
-  }
-
+a .image-container {
+  column-count: 3;
+  column-gap: 0;
+}
+img {
+  max-width: 100%;
+  padding: 5px;
+}
 </style>
